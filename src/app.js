d9767a3ed2207c6105200ebde9178ec0bcd303a5
Victor Lahr <lahr.victor@gmail.com>
2023-04-24 19:52:32 -0300

    chore: configure code for deployment

--- src/app.js
+++ src/app.js
@@ -5,9 +5,6 @@ import express from 'express';
 // VALUE IMPORTS
 import router from './routes/index.route.js';
 
-// GLOBAL CONSTANTS
-const PORT_NUMBER = 5000;
-
 // SERVER CONFIG
 const app = express();
 app.use(express.json());
@@ -15,4 +12,4 @@ app.use(cors());
 app.use(router);
 
 // FUNCTIONS
-app.listen(PORT_NUMBER, () => console.log(`Running server on port ${PORT_NUMBER}!`));
+app.listen(process.env.PORT, () => console.log(`Running server on port ${process.env.PORT}!`));
